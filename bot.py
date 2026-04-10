64d9d7277c24c2be1d978eb74ce678d0569eff03
gammnotreal-lang <gammnotreal@gmail.com>
2026-04-10 22:10:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,36 +2,34 @@ import telebot
 from flask import Flask, request
 import os
 
-# ដាក់ TOKEN និង ID របស់អ្នកឱ្យត្រឹមត្រូវ
 TOKEN = "8726956835:AAGLYnQnVf5ge3yAcsZFXEXu7wrof..."
 MY_ID = 8710784205
 
 bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
+app = Flask(__name__) # ប្ដូរមកប្រើពាក្យ app វិញ
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Bot កំពុងដំណើរការលើ Render រួចរាល់ហើយ!")
+    bot.reply_to(message, "Bot ដំណើរការជោគជ័យហើយ!")
 
 @bot.message_handler(func=lambda message: True)
 def forward_msg(message):
-    info = f"👤 User: {message.from_user.first_name}\n🆔 ID: {message.from_user.id}"
+    info = f"👤 User: {message.from_user.first_name}"
     if message.content_type == 'text':
         bot.send_message(MY_ID, f"{info}\n💬 Message: {message.text}")
-    elif message.content_type == 'photo':
-        bot.send_photo(MY_ID, message.photo[-1].file_id, caption=info)
 
-@server.route('/' + TOKEN, methods=['POST'])
+@app.route('/' + TOKEN, methods=['POST']) # ប្ដូរមកប្រើ app
 def getMessage():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
     return "!", 200
 
-@server.route("/")
+@app.route("/") # ប្ដូរមកប្រើ app
 def webhook():
-    return "Bot is running!", 200
+    bot.remove_webhook()
+    bot.set_webhook(url="https://my-forwarding-bot.onrender.com/" + TOKEN)
+    return "Webhook set successfully!", 200
 
 if __name__ == "__main__":
-    # នេះជាផ្នែកសំខាន់ដើម្បីឱ្យ Render ដើរ (Port)
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000))) # ប្ដូរមកប្រើ app
