485ac1308ac53387cb02c339e3ad73de929e2d1c
gammnotreal-lang <gammnotreal@gmail.com>
2026-04-10 21:31:20 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import telebot
 from flask import Flask, request
 import os
 
-TOKEN = "8726956835:AAGLYnQnVf5ge3yAcsZFXEXu7wrofrqQ_Os"
+# ដាក់ TOKEN និង ID របស់អ្នកឱ្យត្រឹមត្រូវ
+TOKEN = "8726956835:AAGLYnQnVf5ge3yAcsZFXEXu7wrof..."
 MY_ID = 8710784205
 
 bot = telebot.TeleBot(TOKEN)
@@ -10,20 +11,15 @@ server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "សួស្ដី! Bot កំពុងដំណើរការលើ Render។")
+    bot.reply_to(message, "Bot កំពុងដំណើរការលើ Render រួចរាល់ហើយ!")
 
-@bot.message_handler(func=lambda message: message.chat.id != MY_ID, content_types=['text', 'photo', 'sticker'])
+@bot.message_handler(func=lambda message: True)
 def forward_msg(message):
-    username = f"@{message.from_user.username}" if message.from_user.username else "គ្មាន"
-    info = f"👤 User: {message.from_user.first_name}\n📛 Username: {username}\n🆔 ID: {message.from_user.id}"
-    
+    info = f"👤 User: {message.from_user.first_name}\n🆔 ID: {message.from_user.id}"
     if message.content_type == 'text':
         bot.send_message(MY_ID, f"{info}\n💬 Message: {message.text}")
     elif message.content_type == 'photo':
-        bot.send_photo(MY_ID, message.photo[-1].file_id, caption=f"{info}\n🖼 (រូបភាព)")
-    elif message.content_type == 'sticker':
-        bot.send_message(MY_ID, f"{info}\n✨ (Sticker)")
-        bot.send_sticker(MY_ID, message.sticker.file_id)
+        bot.send_photo(MY_ID, message.photo[-1].file_id, caption=info)
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
@@ -34,10 +30,8 @@ def getMessage():
 
 @server.route("/")
 def webhook():
-    bot.remove_webhook()
-    # ចំណាំ៖ Link ខាងក្រោមនេះអ្នកត្រូវប្ដូរក្រោយពេលបង្កើត Web Service ក្នុង Render រួច
-    # bot.set_webhook(url="https://ឈ្មោះ-app-របស់អ្នក.onrender.com/" + TOKEN)
-    return "Bot is Running!", 200
+    return "Bot is running!", 200
 
 if __name__ == "__main__":
+    # នេះជាផ្នែកសំខាន់ដើម្បីឱ្យ Render ដើរ (Port)
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
